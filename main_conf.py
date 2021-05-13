0b9bed14c3e78b713b49b2452e03d9eb2675b066
melandur <account@qunit.ch>
2021-05-13 13:55:11 +0200

    added some conf checks

--- main_conf.py
+++ main_conf.py
@@ -1,5 +1,9 @@
+import os
 import sys
 import json
+from copy import deepcopy
+from collections import Counter
+from loguru import logger as log
 
 
 class ConfigManager:
@@ -32,7 +36,7 @@ class ConfigManager:
                         't1': ['t1.'],
                         't1ce': ['t1ce.'],
                         'flair': ['flair.'],
-                        't2': ['t2.']},
+                        't2': ['t2']},
                     'image_file_type': ['.nii.gz'],
                     'concatenate_image_files': True,
                     'pull_dataset': 'DecathlonDataset',  # 'MedNISTDataset', 'DecathlonDataset', 'CrossValidation'
@@ -82,7 +86,47 @@ class ConfigManager:
                     'test_data_path_store': dict,
                     'copy_manager_': dict,
                 }
-
             }
         else:
             self.params = json.load(load_conf_file_path)
+
+        self.__check_image_search_tag_redundancy()
+        self.__check_image_search_tag_uniqueness()
+        self.__check_and_create_folder_structure()
+
+    def __check_and_create_folder_structure(self):
+        """Check and create folders if they are missing"""
+        folders = [
+            self.params['project']['dataset_store_path'],
+            self.params['project']['result_store_path'],
+            self.params['project']['augmentation_store_path'],
+            self.params['dataset']['folder_path']
+        ]
+        for folder in folders:
+            os.makedirs(folder, exist_ok=True)
+
+    def __check_image_search_tag_redundancy(self):
+        """Check if there are any redundant search tag per image name"""
+        for key, value in self.params['dataset']['image_search_tags'].items():
+            if len(value) != len(set(value)):
+                redundant_tag = list((Counter(value) - Counter(list(set(value)))).elements())
+                log.error(f'The image search tag {redundant_tag} appears multiple times for the image name {key}')
+                exit(1)
+
+    def __check_image_search_tag_uniqueness(self):
+        """Check if the image search tags are unique enough to avoid wrong data loading"""
+        tags = [*self.params['dataset']['image_search_tags'].values()]
+        tags = [x for sublist in tags for x in sublist]  # flatten nested list
+        for i, tag in enumerate(tags):
+            tmp_tags = deepcopy(tags)
+            tmp_tags.pop(i)
+            if [x for x in tmp_tags if x in tag]:
+                vague_tag = [x for x in tmp_tags if x in tag]
+                log.error(f'The image search tag {vague_tag} is not expressive/unique enough. '
+                          f'Try to add additional information to the search tag like "_", "."')
+                exit(1)
+
+
+if __name__ == '__main__':
+    cm = ConfigManager()
+
