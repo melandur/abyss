b679fe5028311bb120cd5478cbe52bca76580f04
melandur <you@example.com>
2022-06-09 22:31:38 +0200

    fix

--- abyss/config_file.py
+++ abyss/config_file.py
@@ -130,5 +130,5 @@ class ConfigFile:
             'post_processing': {},
         }
 
-        def __str__(self):
-            return self.__class__.__name__
+    def __str__(self):
+        return self.__class__.__name__
